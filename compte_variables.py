9066c873d83f1d170297bc9c6357e1ef00228dad
Applegate Augustin <augustin.applegate@student-cs.fr>
2018-11-19 16:21:36 +0100

    manip git

--- compte_variables.py
+++
@@ -1,7 +0,0 @@
-def countVariables(filePath):
-    with open (filePath,'r') as file :
-        fileList=list(file)
-        nombreDeVariables=0
-        for i in range (len(fileList)):
-            nombreDeVariables=nombreDeVariables+list(fileList[i]).count('=')
-    return nombreDeVariables
