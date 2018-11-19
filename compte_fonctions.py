2e4a1cfc5101e8be15176a83eea65a19e634f050
Jean-Baptiste Hatt <jeanbaptiste.hatt@student-cs.fr>
2018-11-19 15:09:42 +0100

    première fonction

--- compte_fonctions.py
+++ compte_fonctions.py
@@ -1,9 +1,9 @@
 def compte_fonctions(path):
 	i=0
-	with open('path','r') as code:
+	with open(path,'r') as code:
 		code2=list(code)
 		for k in code2 :
-	        	if 'def' in str(k):
+			if 'def' in k:
 				i=i+1
 	print (i)
 compte_fonctions('code_candidat.txt')
