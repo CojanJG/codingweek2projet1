0a54fb07d10535404c4bd56f980a66d8c7c46c85
Jean-Baptiste Hatt <jeanbaptiste.hatt@student-cs.fr>
2018-11-19 16:59:05 +0100

    Nom des fonctions

--- compte_fonctions.py
+++ compte_fonctions.py
@@ -1,9 +1,20 @@
 def compte_fonctions(path):
 	i=0
+	l=[]
 	with open(path,'r') as code:
 		code2=list(code)
 		for k in code2 :
+			a=0
+			b=0
 			if 'def' in k:
 				i=i+1
-	print (i)
-compte_fonctions('code_candidat.txt')
+				k_list=list(k)
+				def_list=list('def')
+				a=k_list.index('f')
+				if '(' in k:
+					b=k_list.index('(')
+					l.append(k[a+1:b])
+				else:
+					l.append(k[a+1:])
+	print (i,l)
+compte_fonctions('code_candidat1.txt')
