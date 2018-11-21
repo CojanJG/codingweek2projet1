20d14f04ddb6b32f2f073a0daa508a80e4a24daa
Applegate Augustin <augustin.applegate@student-cs.fr>
2018-11-21 15:33:21 +0100

    correction mvp2

--- MVP2/doublon.py
+++ MVP2/doublon.py
@@ -5,7 +5,21 @@ def doublon(path):
         code_list=list(code)
         result=[]
         for i in range (len(code_list)):
-            print(result)
+            #suppression des espaces initiaux
+            code_list[i]=list(code_list[i])
+            m=0
+            while m<len(code_list[i]) and code_list[i][m]==' ':
+                code_list[i].pop(0)
+                #on supprime les espaces
+                m+=1
+            for m in range(1,len(code_list[i])):
+                #on repasse à un seul string en concatenant notre liste de string et le mot se retrouve en position 0
+                code_list[i][0]=code_list[i][0]+code_list[i][m]
+            while len(code_list[i])>1:
+                code_list[i].pop(-1)
+            if len(code_list[i])>0:
+                code_list[i]=code_list[i][0]
+                #on passe d'une liste de string à un string : ["machin"] -> "machin"
             if code_list[i] in code_list[i:]:    #on retrouve cette ligne dans les lignes qui suivent
                 #on vérifie si cette ligne n'a pas déjà été compté comme redondante
                 j=-1
@@ -16,6 +30,6 @@ def doublon(path):
                     result[j][1]+=1
                 else:
                     result.append([code_list[i],2])
-        return result,len(result)
+        return result
 
-print (doublon("test.txt"))
+print (doublon("event_candidate_b.rb"))
