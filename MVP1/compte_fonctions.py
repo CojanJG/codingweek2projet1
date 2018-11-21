80fef95eed6c5c051bdb988d81c8e83a1f383f04
Jean-Baptiste Hatt <jeanbaptiste.hatt@student-cs.fr>
2018-11-21 11:43:38 +0100

    21/11/18

--- MVP1/compte_fonctions.py
+++ MVP1/compte_fonctions.py
@@ -1,6 +1,10 @@
 def compte_fonctions(path):
+	from nltk.corpus import wordnet
 	i=0
 	l=[]
+	n=0
+	m=0
+	a=0
 	with open(path,'r') as code:
 		code2=list(code)
 		for k in code2 :
@@ -13,8 +17,18 @@ def compte_fonctions(path):
 				a=k_list.index('f')
 				if '(' in k:
 					b=k_list.index('(')
-					l.append(k[a+1:b])
+					l.append(k[a+2:b])
 				else:
-					l.append(k[a+1:])
-	print (i,l)
+					l.append(k[a+2:])
+	for j in range(len(l)):
+		if not wordnet.synsets(l[j]):
+			 #Not an English Word
+			n=n+1
+		else:
+			#English Word
+			m=m+1
+	print ('nombre de fonctions:',i)
+	print (l)
+	print ('nombre de fonctions mal nommées:',n)
+	print ('nombre de fonctions bien nommées:',m)
 compte_fonctions('code_candidat1.txt')
