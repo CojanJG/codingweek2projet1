0f471590cba9d8959928d8db18c9a84db5a6f361
AlexandreDubourg <alexandre.dubourg@student.ecp.fr>
2018-11-22 14:54:10 +0100

    mvp3

---
+++ MVP3/noms_variables.py
@@ -0,0 +1,33 @@
+def nomsVariables(filePath):
+    #renvoie la liste des noms des varibles utilisées
+    with open (filePath,'r') as file :
+        fileList=list(file)
+        noms=[]
+        for i in range (len(fileList)):
+            if " = " in fileList[i]:
+                caracteres = list(fileList[i])
+                k=caracteres.index("=")
+                l=k-2
+                while fileList[i][l]!=' ':
+                    l=l-1
+                noms.append(fileList[i][l+1:k-1])
+    return (noms)
+
+
+
+import enchant
+def testNoms (noms):
+    #renvoi un duet avec le nombre de variables bien nommées et le nombre de variables mal nommées
+    d = enchant.Dict("en_US")
+    bonnesVariables = 0
+    mauvaisesVariables = 0
+    print(len(noms))
+    for i in range (len(noms)):
+        if d.check(noms[i]) :
+            bonnesVariables=bonnesVariables + 1
+        else :
+            mauvaisesVariables=mauvaisesVariables + 1
+    return([bonnesVariables, mauvaisesVariables])
+    
+
+
